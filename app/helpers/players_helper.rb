8848382fc09116df549cfc6ae020ef689ed70022
huzefa <huzefa.biyawarwala@sell.do>
2021-05-23 12:19:47 +0530

    fixed new player creation issue and added CSRF token for security

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -2,10 +2,14 @@ module PlayersHelper
   def get_high_score_details(player)
     participant = player.participants.order(player_total_score: :desc).first
 
-    highest_score = participant.player_total_score
-    game = participant.game
-    against_player = game.participants.where("player_id != ?", player.id).first.player.full_name
+    if participant.present?
+      highest_score = participant&.player_total_score
+      game = participant.game
+      against_player = game.participants.where("player_id != ?", player.id).first.player.full_name
 
-    [highest_score, against_player, game.created_at.strftime("%B %d %Y")]
+      [highest_score, against_player, game.created_at.strftime("%B %d %Y")]
+    else
+      'NA'
+    end
   end
 end
